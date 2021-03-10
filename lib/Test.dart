3ef351c3bc48a92ba75ee929049cee28cb5d2bd1
我吃大西瓜 <706375160@qq.com>
2021-03-10 18:33:33 +0800

    update: 父widget管理widget的state

--- lib/Test.dart
+++ lib/Test.dart
@@ -1,139 +1,82 @@
-import 'dart:ui';
+// TapboxA manages its own state.
+
+// TapboxA 管理自身状态.
+
+//------------------------- TapboxA ----------------------------------
 
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
-void main() => runApp(FavoriteWidget());
+void main() => runApp(MyApp());
+//------------------------- MyApp ----------------------------------
 
-class FavoriteWidget extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() => _FavoriteWidgetState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+        ),
+        body: Center(
+          child: TapboxA(),
+        ),
+      ),
+    );
+  }
 }
 
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  bool _isFavorite = true;
-  int _favoriteCount = 41;
+class TapboxA extends StatefulWidget {
+  TapboxA({Key key}) : super(key: key);
 
-  void _toggleFavorite() {
+  @override
+  _TapboxAState createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    print('回调 _handleTapboxChanged');
     setState(() {
-      if (_isFavorite) {
-        _favoriteCount--;
-        _isFavorite = false;
-      } else {
-        _favoriteCount++;
-        _isFavorite = true;
-      }
+      _active = newValue;
     });
+  }
+
+  Widget build(BuildContext context) {
+    return TapboxB(
+      active: _active,
+      onChanged: _handleTapboxChanged,
+    );
+  }
+}
+
+class TapboxB extends StatelessWidget {
+  TapboxB({this.active, @required this.onChanged});
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    print('回调_handleTap');
+    onChanged(!active);
   }
 
   @override
   Widget build(BuildContext context) {
-    //图像区域
-    var icon = Container(
-      child: Image.asset(
-        'images/pavlova.jpg',
-        width: 600,
-        height: 240,
-        fit: BoxFit.cover,
-      ),
-    );
-
-    //文本标题区域
-    var titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    'Oeschinen Lake Campground',
-                    style: TextStyle(fontWeight: FontWeight.w500),
-                  ),
-                ),
-                Text(
-                  'Kandersteg, Switzerland',
-                  style: TextStyle(color: Colors.grey[500]),
-                )
-              ],
-            ),
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
           ),
-          IconButton(
-              icon: _isFavorite
-                  ? Icon(
-                      Icons.star,
-                      color: Colors.red,
-                    )
-                  : Icon(
-                      Icons.star_border,
-                      color: Colors.red,
-                    ),
-              onPressed: _toggleFavorite),
-          Text('$_favoriteCount')
-        ],
-      ),
-    );
-
-    Widget _buildButtonColumn(Color color, IconData icon, String label) {
-      return Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            icon,
-            color: color,
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 8),
-            child: Text(
-              label,
-              style: TextStyle(
-                  fontSize: 12, fontWeight: FontWeight.w400, color: color),
-            ),
-          )
-        ],
-      );
-    }
-
-    Color color = Theme.of(context).primaryColor;
-    //按钮区域
-    Widget buttonSection = Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          _buildButtonColumn(color, Icons.call, 'CALL'),
-          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-          _buildButtonColumn(color, Icons.share, 'SHARE'),
-        ],
-      ),
-    );
-
-    //文本内容区域
-    Widget textSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-        'Alps. Situated 1,578 meters above sea level, it is one of the '
-        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-        'half-hour walk through pastures and pine forest, leads you to the '
-        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-        'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-        textAlign: TextAlign.start,
-      ),
-    );
-
-    return MaterialApp(
-      title: 'Hello World',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
         ),
-        body: ListView(
-          children: [icon, titleSection, buttonSection, textSection],
-        ),
+        width: 200.0,
+        height: 200.0,
+        color: active ? Colors.lightGreen[700] : Colors.grey[600],
       ),
     );
   }
