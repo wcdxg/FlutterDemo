0197f089fc5750ff7fb1e058a00872cd0e32ff6e
我吃大西瓜 <706375160@qq.com>
2021-03-12 15:21:10 +0800

    update: Animation.dart

---
+++ lib/Layout/Animation.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(LogoApp());
+
+class LogoApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _LogoAppState();
+}
+
+class _LogoAppState extends State<LogoApp> {
+  Animation<double> animation;
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center();
+  }
+}
